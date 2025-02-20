784cf8efe7d8e82a600f76b5c629ba9c54697d82
ZoZo-182 <robinsoz@clarkson.edu>
2025-02-20 00:05:30 -0500

    broken tests for CI to check for failures

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,22 @@
+package auth
+
+import (
+    "reflect"
+    "testing"
+    "net/http"
+)
+
+func TestGetAPIKey(t *testing.T) {
+    header := http.Header{} // Empty header (no auth header)
+
+    got, err := GetAPIKey(header)
+    //    wantErr := ErrNoAuthHeaderIncluded
+
+    if !reflect.DeepEqual(wantErr, err) {
+        t.Fatalf("Expected: %v, got: %v", wantErr, err)
+    }
+
+    if got != "" {
+        t.Fatalf("Expected empty API key, got %q", got)
+    }
+}
